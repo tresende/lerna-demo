6fc4d7208c5dbb315fb0317bf32b7568d86deb75
Thiago Resende <thiago.gcresende@gmail.com>
2021-10-12 15:04:11 -0300

    add lerna setup

---
+++ packages/common/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button textColor="red">Click Me!</Button>
+    </div>
+  );
+}
+
+export default App;
